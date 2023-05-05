14fae25707f1b359033516bcb135c9c810faa08c
Nicollas Prudencio <67238467+nic37@users.noreply.github.com>
2023-05-05 02:21:58 -0300

    Convertido para Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
+import styled from 'styled-components'
+import EstiloGlobal from './styles'
 
-import './global.css'
+const Body = styled.body`
+  padding-bottom: 120px;
+`
+const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media (max-width: 1024px) {
+    max-width: 80%;
+  }
+`
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <div className="container">
-        <ListaVagas />
-      </div>
+      <EstiloGlobal />
+      <Body>
+        <Header />
+        <Hero />
+        <Container>
+          <ListaVagas />
+        </Container>
+      </Body>
     </>
   )
 }
